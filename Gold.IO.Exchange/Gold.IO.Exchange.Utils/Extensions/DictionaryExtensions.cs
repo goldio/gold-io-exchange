874fd7a1eb23b1a7a3e57a2d4161b2e667605954
Profiteam <31069986+Profiteam@users.noreply.github.com>
2018-12-29 16:29:01 +0300

    extensions

---
+++ Gold.IO.Exchange/Gold.IO.Exchange.Utils/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utils.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static String ToQueryString(this IDictionary<String, String> values)
+        {
+            return String.Join("&", values.Select(x => $"{x.Key}={x.Value}"));
+        }
+    }
+}
---
+++ Gold.IO.Exchange/Gold.IO.Exchange.Utils/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToTitleCase(this string str)
+        {
+            return str.Substring(0, 1).ToUpper() + str.Substring(1).ToLower();
+        }
+    }
+}
